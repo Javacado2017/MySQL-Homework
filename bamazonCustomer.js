e648423da81aaedff5a64d629c42bc288038dbe1
unknown <newtonsapple2017@gmail.com>
2018-05-15 18:05:11 -0400

    added photos and description for the lines of code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,12 @@ var mysql = require('mysql');
 require('console.table');
 var inquirer = require('inquirer');
 
+//Global variables for general use
+var idIndex = ''
+var quantityBought = ''
+var quantityRemaining = ''
+var totalCost = ''
+
 //Creates a connection object
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -11,7 +17,6 @@ var connection = mysql.createConnection({
     password: '',
     database: 'bamazon_db'
 });
-
 
 //Creates a connection to the database
 connection.connect(function(error){
@@ -22,29 +27,27 @@ connection.connect(function(error){
     console.log('\n' + 'Welcome the Bamazon Store Front. The following products are currently listed for sale: \n')
 });
 
-
-//Lists the current products available
+//Lists the current products available and calls on bamazonPurchase() to make a purchase. 
 function listProducts(){
     connection.query('SELECT * FROM products' , function (error, results){
-        console.table(results);        
+        console.table(results);
+        //Runs function to make a purchase
         bamazonPurchase(results);
     });
 }
 listProducts();
 
-var idIndex = ''
-var quantityBought = ''
-var quantityRemaining = ''
-var totalCost = ''
 
 //Function to make a purchase
 function bamazonPurchase(results) {
 
+    //Asks the User what product and qty to purchase. 
     inquirer.prompt([
         {
             type: 'input',
-            message: 'To purchase, input Product ID from above.',
+            message: 'What would you like to purchase? Input product id and press enter to continue.',
             name: 'productID',
+            //Validates that the Product ID exists in the database
             validate: function validateProductID(name) {   
                 var productCheck = [];
 
@@ -54,6 +57,7 @@ function bamazonPurchase(results) {
                 };
                 
                 if (productCheck.includes(parseInt(name))) {
+                //Stores values for later use
                 idIndex = name
                 return name !== '';
                 } else {
@@ -63,14 +67,15 @@ function bamazonPurchase(results) {
         },
         {
             type: 'input',     
-            message: 'Input valid quantity.',
+            message: 'How many units would you like to purchase? See table above, then press enter to make purchase.',
             name: 'purchaseQuantity',
+            //Validates that the quantiy desired exists in the database
             validate: function validatePurchaseQuantity(name) {
                 
                 var quanityAvailable = results[(idIndex-1)].stocked_quantity
                 
-
                 if (quanityAvailable >= name) {
+                //Stores values for later use
                 quantityBought = name;
                 quantityRemaining = quanityAvailable - quantityBought;
                 return name !=='';
@@ -79,17 +84,23 @@ function bamazonPurchase(results) {
                 }                
             }
         },
-//confirm purchase. If yes, make purchase and continue, if no,the rerun the script
+
+        //Confirms purchase. (Maybe I'll code this later.)
+
     ]).then(function(inputs) {
-        totalValue = results[(idIndex-1)].price_$*quantityBought;
+        //Calculates and stored the total cost of the purchase 
+        totalCost = results[(idIndex-1)].price_$*quantityBought;
 
-        console.log('You just purchased ' + results[(idIndex-1)].product_name + ' at $' + 
-        results[(idIndex-1)].price_$ + 'per unti for a total of $' + totalValue + '. Thank you. Goodbye.');
+        //Gives summary to of purchase
+        console.log('You purchased ' + quantityBought + ' units of ' + results[(idIndex-1)].product_name + ' at $' + 
+        results[(idIndex-1)].price_$ + ' per unit. A total of $' + totalCost + ' was charged to your account. Thank you. Goodbye.');
 
+        //Updates database with new quantity
         updateDatabase();
     });
 }
 
+//Function to update database and end connection
 function updateDatabase(results) {
     connection.query('UPDATE products SET stocked_quantity = ' + quantityRemaining + ' WHERE id =' + idIndex, function(error, results){
         if (error) {
